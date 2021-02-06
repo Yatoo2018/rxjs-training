de9f5e9e6bf2fdb755c6cc7c3466ab4fc635b150
tanpengfei <tanpengfei1@100tal.com>
2021-02-06 11:36:06 +0800

    commit my solution

--- 01/imperative-array.js
+++ 01/imperative-array.js
@@ -5,5 +5,6 @@ var source = ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'];
 // TODO: Create a var `result` that contains the sum
 // of all numbers in source. Use a traditional for loop
 // for this exercise.
-
+let result = 0
+result = source.filter(item => !isNaN(item)).reduce((sum, item) => sum - (-item), result)
 console.log(result); // should print the number `33` only
